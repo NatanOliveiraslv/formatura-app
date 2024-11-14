0c2750fddcfd7ae0f4552d7b5c0ed2ca312ce843
NatanOliveiraslv <natanoliveiraslv14@gmail.com>
2024-11-13 21:35:00 -0300

    mysql

--- formaturaapp/settings.py
+++ formaturaapp/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'formaturaapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
