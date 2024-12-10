57fbdcd2d9873d6f5be10a62c8de61292f7012e4
NatanOliveiraslv <natanoliveiraslv14@gmail.com>
2024-12-10 00:29:02 -0300

    add: odem by

--- apps/presentes/views.py
+++ apps/presentes/views.py
@@ -4,7 +4,20 @@ from django.contrib import messages
 from django.shortcuts import redirect, render
 
 def presentes(request):
-    presentes = Presentes.objects.all()
+    ordenacao = request.GET.get('ordem')  # Padrão: A-Z pelo nome
+    
+    # Define a ordenação com base no parâmetro
+    if ordenacao == 'valor_asc':
+        presentes = Presentes.objects.order_by('value')  # Menor para maior
+    elif ordenacao == 'valor_desc':
+        presentes = Presentes.objects.order_by('-value')  # Maior para menor
+    elif ordenacao == 'nome_asc':
+        presentes = Presentes.objects.order_by('title')  # A-z
+    elif ordenacao == 'nome_desc':
+        presentes = Presentes.objects.order_by('-title')  # Z-A
+    else: 
+        presentes = Presentes.objects.all()
+        
     # Formatar o valor de 'value' com ponto decimal para cada presente
     for presente in presentes:
         presente.value = "{:.2f}".format(presente.value).replace(',', '.')  # Garante ponto ao invés de vírgula
